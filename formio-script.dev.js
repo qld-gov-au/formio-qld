465425d615d184433058ce78ce0963e608b4785f
Joshua Miller <Joshua.Miller@smartservice.qld.gov.au>
2026-07-20 15:28:27 +1000

    console git bridge checker

--- formio-script.dev.js
+++ formio-script.dev.js
@@ -77,7 +77,7 @@ const createScripts = (scripts, i, mainResolve) => {
       document.body.appendChild(elem);
       elem.onload = () => {
         console.info("FormioScript loaded:", src || href);
-        console.log("BOING");
+        console.info("BOING");
         resolve();
       };
     });
